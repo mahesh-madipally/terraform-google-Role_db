60c3aa6bf4b711b1ee15bd00c7522d312a3623c1
Madipally <mahesh.madipally@accenture.com>
2021-07-29 12:59:35 +0530

    modified the template as predefined in main.tf

--- main.tf
+++ main.tf
@@ -60,7 +60,6 @@ resource "google_project_iam_custom_role" "project-predefined-role" {
   role_id     = var.role_id
   title       = var.title
   description = var.description
-  base_roles  = var.base_roles
   permissions = local.permissions
 }
 
